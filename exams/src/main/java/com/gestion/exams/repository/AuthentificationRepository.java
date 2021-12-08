56891a1ad579184067018a09a3ec9fbd5f8c4704
Anthony Lemaire <anthony_pro1998@outlook.fr>
2021-12-08 10:58:27 +0100

    rename authentificationRepository

--- exams/src/main/java/com/gestion/exams/repository/AuthentificationRepository.java
+++ exams/src/main/java/com/gestion/exams/repository/AuthentificationRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.gestion.exams.entity.Authentification;
 
 @Repository
-public interface AuthentificiationRepository extends JpaRepository<Authentification, String>{
+public interface AuthentificationRepository extends JpaRepository<Authentification, String>{
 
 }
