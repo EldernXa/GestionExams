2a6cafa997da8f9d8b2581f68f0042b3e4e259ee
Anthony Lemaire <anthony_pro1998@outlook.fr>
2021-12-08 13:13:43 +0100

    fix pk for inscription

---
+++ exams/src/main/java/com/gestion/exams/entity/InscriptionPK.java
@@ -0,0 +1,53 @@
+package com.gestion.exams.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class InscriptionPK implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne
+	@JoinColumn
+	private UE ue;
+
+	@ManyToOne
+	@JoinColumn
+	private Student student;
+
+	public InscriptionPK() {
+		super();
+	}
+
+	public InscriptionPK(UE ue, Student student) {
+		super();
+		this.ue = ue;
+		this.student = student;
+	}
+
+	public UE getUe() {
+		return ue;
+	}
+
+	public void setUe(UE ue) {
+		this.ue = ue;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+
+}
