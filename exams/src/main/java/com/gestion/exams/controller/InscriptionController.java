bc4c07c4c61d998031a907bdfa2a9fd559e4edd5
SalemDoub <salem.doub2016@etu.univ-amu.fr>
2021-12-09 23:06:32 +0100

    show/register/unsubscribe Inscriptions for a Student //SERVICE+CONTROLLER

--- exams/src/main/java/com/gestion/exams/controller/InscriptionController.java
+++ exams/src/main/java/com/gestion/exams/controller/InscriptionController.java
@@ -1,10 +1,36 @@
 package com.gestion.exams.controller;
 
+import com.gestion.exams.entity.Inscription;
+import com.gestion.exams.entity.Student;
+import com.gestion.exams.entity.UE;
+import com.gestion.exams.services.InscriptionService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/inscription")
 public class InscriptionController {
+
+    InscriptionService inscriptionService ;
+
+@GetMapping("/all")
+public List<Inscription> showInscriptionsOfStudent(Student student){
+    return inscriptionService.getInscriptionsOfStudent(student);
+}
+@PostMapping("/register")
+public Inscription registerStudentToUE(Student student , int year , UE ue){
+    return inscriptionService.registerStudentToUE(student,year,ue);
+}
+
+@DeleteMapping("/unsubscribe")
+public void unsubscribeStudentFromInscription(Student student ,Inscription inscription){
+    inscriptionService.unsubscribeStudentFromInscription(student,inscription);
+}
+
+
+
+
 
 }
