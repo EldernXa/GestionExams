5bab9edf1661de6039ddf645b9a406c33dcd0147
bardaghjigeorges <georges.bardaghji@etu.univ-amu.fr>
2021-12-11 17:40:18 +0100

    implementation of the UE Service

--- exams/src/main/java/com/gestion/exams/services/UEService.java
+++ exams/src/main/java/com/gestion/exams/services/UEService.java
@@ -1,8 +1,36 @@
 package com.gestion.exams.services;
 
+import com.gestion.exams.entity.UE;
+import com.gestion.exams.repository.UERepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UEService {
 
+    @Autowired
+    UERepository ueRepository;
+
+    public UE createUE(UE ue){
+        return ueRepository.save(ue);
+    }
+
+    public UE updateUE(UE ue, UE ue1){
+        ue.setName(ue1.getName());
+        ue.setCredit(ue1.getCredit());
+        ue.setDurationExam(ue1.getDurationExam());
+        ue.setDiscipline(ue1.getDiscipline());
+        return ueRepository.save(ue);
+    }
+
+    public void deleteUE(String name){
+        UE ue = ueRepository.getUEByName(name);
+        ueRepository.delete(ue);
+    }
+
+    public List<UE> getAllUE(){
+        return ueRepository.findAll();
+    }
 }
