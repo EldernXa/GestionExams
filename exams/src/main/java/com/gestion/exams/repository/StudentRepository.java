32e6aae26c359ce4165a9462300f855d77766768
Thibaut <thi6aut@yahoo.fr>
2021-12-11 16:21:52 +0100

    implementation of a student service : getStudentsByExamId (will be used by grade service)

--- exams/src/main/java/com/gestion/exams/repository/StudentRepository.java
+++ exams/src/main/java/com/gestion/exams/repository/StudentRepository.java
@@ -1,11 +1,18 @@
 package com.gestion.exams.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.gestion.exams.entity.Student;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long>{
+
+    @Query("SELECT i.inscriptionPK.student FROM Inscription i, Exam e WHERE ( i.inscriptionPK.ue = e.ue AND e.idExam = :searchValue ) ")
+    List<Student> findStudentByExamId(@Param("searchValue") long idExam);
 
 }
--- exams/src/main/java/com/gestion/exams/services/StudentService.java
+++ exams/src/main/java/com/gestion/exams/services/StudentService.java
@@ -1,8 +1,21 @@
 package com.gestion.exams.services;
 
+import com.gestion.exams.entity.Grade;
+import com.gestion.exams.entity.Student;
+import com.gestion.exams.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    public List<Student> getStudentsByExamId(long idExam){
+        return studentRepository.findStudentByExamId(idExam);
+    }
 
 }
