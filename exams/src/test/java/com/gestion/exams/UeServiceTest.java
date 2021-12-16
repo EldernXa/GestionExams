ea01e5e5d66936d515cb4dd6f226decc46aba6ee
bardaghjigeorges <georges.bardaghji@etu.univ-amu.fr>
2021-12-16 15:35:54 +0100

    init of mockito and implementing test for CreateUE and getALLUeTest

--- exams/src/test/java/com/gestion/exams/UeServiceTest.java
+++ exams/src/test/java/com/gestion/exams/UeServiceTest.java
@@ -1,31 +1,78 @@
 package com.gestion.exams;
 
+import com.gestion.exams.entity.Discipline;
+import com.gestion.exams.entity.UE;
+import com.gestion.exams.repository.UERepository;
 import com.gestion.exams.services.UEService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import javax.transaction.Transactional;
 
-@SpringBootTest
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+//@SpringBootTest
 @Transactional
+@RunWith(MockitoJUnitRunner.class)
 public class UeServiceTest {
-    @Autowired
+    @InjectMocks
     UEService ueService;
+    @Mock
+    private UERepository ueRepository;
 
-    @Test
-    public void createUeTest(){
+    UE ue1 = new UE();
+    UE ue2 = new UE();
 
+    @BeforeAll
+    public void initUeForTest(){
+        ue1.setName("ASR");
+        ue1.setCredit(3);
+        ue1.setDurationExam(2);
+        ue1.setDiscipline(Discipline.INFORMATIQUE);
+
+        ue2.setName("Analyse");
+        ue2.setCredit(6);
+        ue2.setDurationExam(2);
+        ue2.setDiscipline(Discipline.MATH);
+
+        ueRepository.save(ue1);
+        ueRepository.save(ue2);
     }
 
-    @Test
+    @org.junit.Test
+    public void createUeTest(){
+        when(ueRepository.save(ArgumentMatchers.any(UE.class))).thenReturn(ue1);
+        UE createdUE = ueService.createUE(ue1);
+        assertEquals(createdUE.getName(), ue1.getName());
+        verify(ueRepository).save(ue1);
+    }
+
+    @org.junit.Test
     public void updateUeTest(){
 
     }
 
     @Test
     public void getAllUeTest(){
-
+        List<UE> ues = new ArrayList<>();
+        ues.add(ue1);
+        ues.add(ue2);
+        given(ueRepository.findAll()).willReturn(ues);
+        List<UE> expected  = ueService.getAllUE();
+        assertEquals(expected, ues);
+        verify(ueRepository).findAll();
     }
 
     @Test
