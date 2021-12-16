89a048d2946a375e3cf19b161f668cb0d3c44c8d
bardaghjigeorges <georges.bardaghji@etu.univ-amu.fr>
2021-12-16 10:26:05 +0100

    initialization of the UE test class

---
+++ exams/src/test/java/com/gestion/exams/UeServiceTest.java
@@ -0,0 +1,40 @@
+package com.gestion.exams;
+
+import com.gestion.exams.services.UEService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+@SpringBootTest
+@Transactional
+public class UeServiceTest {
+    @Autowired
+    UEService ueService;
+
+    @Test
+    public void createUeTest(){
+
+    }
+
+    @Test
+    public void updateUeTest(){
+
+    }
+
+    @Test
+    public void getAllUeTest(){
+
+    }
+
+    @Test
+    public void getUeByNameTest(){
+
+    }
+
+    @Test
+    public void deleteUeTest(){
+
+    }
+}
