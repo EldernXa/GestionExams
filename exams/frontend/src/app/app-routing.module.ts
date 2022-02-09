9828eee83e5abd7000167a402e1f37fc34fa590a
Anthony LEMAIRE <anthony_pro1998@outlook.fr>
2022-02-09 16:48:06 +0100

    remove a routing useless

--- exams/frontend/src/app/app-routing.module.ts
+++ exams/frontend/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { PeriodListComponent } from './component/period-list/period-list.component';
 import { PeriodFormComponent } from './component/period-form/period-form.component';
 import { PeriodDetailsComponent } from './component/period-details/period-details.component';
-import { ExamManagementComponent } from './component/exam-management/exam-management.component';
 import {GradeListComponent} from "./component/grade-list/grade-list.component";
 import {UeListComponent} from "./component/ue-list/ue-list.component";
 import {UeFormComponent} from "./component/ue-form/ue-form.component";
@@ -15,7 +14,6 @@ const routes: Routes = [
   { path: 'period', component: PeriodListComponent},
   { path: 'addperiod', component: PeriodFormComponent},
   { path: 'period/:id', component: PeriodDetailsComponent},
-  { path: 'periodManagement/:id', component: ExamManagementComponent},
   { path: 'grades/exam/:id', component: GradeListComponent},
   { path: 'ue', component: UeListComponent},
   { path: 'addue', component: UeFormComponent},
