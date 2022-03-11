ee83ed9707fa5ae5a3c7004bd184cbd92b8cc4f1
Anthony <anthony_pro1998@outlook.fr>
2022-03-11 13:14:40 +0100

    display error when bad ident or mdp

--- exams/frontend/src/app/component/login/login.component.ts
+++ exams/frontend/src/app/component/login/login.component.ts
@@ -1,9 +1,8 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { catchError, map, of, retryWhen, shareReplay, tap, throwError } from 'rxjs';
+import { catchError, of} from 'rxjs';
 import { LoginService } from 'src/app/service/login/login.service';
-import { PeriodService } from 'src/app/service/period/period-service.service';
 
 @Component({
   selector: 'app-login',
@@ -26,28 +25,41 @@ export class LoginComponent implements OnInit {
 
   onSubmit(){
     let canConnect: boolean = true;
-    this.loginService.loginToken(this.ident, this.mdp).subscribe((data)=>{
-      this.loginService.loginRole(data).pipe(
-        catchError(error => {
-          canConnect = false;
-          switch(error.status){
-            case 410 :{
-              this.msg = "Erreur survenue lors de la vérification du token."
-              break;
-            }
-            default:{
-              this.msg = "Erreur inconnue."
-            }
-          }
-          return of([]);
-        })
-    )
-      .subscribe((data) => {
-        if(canConnect){
-          localStorage.setItem("role", String(data));
-          this.loginService.moveOnIndexPage();
+    this.loginService.loginToken(this.ident, this.mdp).pipe(
+      catchError(error => {
+        canConnect = false;
+        if(error.status == 403){
+          this.msg = "Mauvais identifiant ou mot de passe.";
+        }else{
+          this.msg = "Erreur inconnue lors de la tentative de connexion."
         }
-      });;
+        return of([]);
+      })
+    )
+    .subscribe((dataToken)=>{
+      if(canConnect){
+        this.loginService.loginRole(dataToken.toString()).pipe(
+          catchError(error => {
+            canConnect = false;
+            switch(error.status){
+              case 410 :{
+                this.msg = "Erreur survenue lors de la vérification du token."
+                break;
+              }
+              default:{
+                this.msg = "Erreur inconnue lors de la tentative de connexion."
+              }
+            }
+            return of([]);
+          })
+      )
+        .subscribe((data) => {
+          if(canConnect){
+            localStorage.setItem("role", String(data));
+            this.loginService.moveOnIndexPage();
+          }
+        });
+    }
     });
   }
 
