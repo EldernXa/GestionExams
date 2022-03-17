fcc97fce9c7a57a53ed32e4f72bb5ec2f4070c97
Anthony <anthony_pro1998@outlook.fr>
2022-03-17 08:35:06 +0100

    change the display format for duration of the exam

--- exams/frontend/src/app/component/inscriptions-list/inscriptions-list.component.ts
+++ exams/frontend/src/app/component/inscriptions-list/inscriptions-list.component.ts
@@ -17,15 +17,26 @@ export class InscriptionsListComponent implements OnInit {
     this.loginService.redirectIfNotLogin();
     this.inscriptions = [];
     this.inscriptionsService.findAll().subscribe(data=>{
-      this.inscriptions = data;
+      //this.inscriptions = data;
       this.inscriptions = this.inscriptions.sort((a,b) => b.year - a.year);
+      for(var inscription of data){
+        inscription.ue.durationExam = Number(inscription.ue.durationExam)/60;
+        this.inscriptions.push(inscription);
+      }
     });
   }
 
   ngOnInit(): void {
     this.inscriptionsService.findAll().subscribe(data=>{
-      this.inscriptions = data;
+      //this.inscriptions = data;
       this.inscriptions = this.inscriptions.sort((a,b) => b.year - a.year);
+      for(var inscription of data){
+        console.log(inscription.ue.durationExam);
+        console.log(Number(inscription.ue.durationExam)/60);
+        inscription.ue.durationExam = Number(inscription.ue.durationExam)/60;
+        console.log(inscription.ue.durationExam);
+        this.inscriptions.push(inscription);
+      }
     });
   }
 
