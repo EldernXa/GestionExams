c5fe7cd86f99fabbcd11aa49aaf8aa5cad7852eb
Anthony <anthony_pro1998@outlook.fr>
2022-03-17 08:36:28 +0100

    clear the code in inscription student view

--- exams/frontend/src/app/component/inscriptions-list/inscriptions-list.component.ts
+++ exams/frontend/src/app/component/inscriptions-list/inscriptions-list.component.ts
@@ -16,25 +16,13 @@ export class InscriptionsListComponent implements OnInit {
   constructor(private route: ActivatedRoute, private inscriptionsService: InscriptionsService, private loginService:LoginService) {
     this.loginService.redirectIfNotLogin();
     this.inscriptions = [];
-    this.inscriptionsService.findAll().subscribe(data=>{
-      //this.inscriptions = data;
-      this.inscriptions = this.inscriptions.sort((a,b) => b.year - a.year);
-      for(var inscription of data){
-        inscription.ue.durationExam = Number(inscription.ue.durationExam)/60;
-        this.inscriptions.push(inscription);
-      }
-    });
   }
 
   ngOnInit(): void {
     this.inscriptionsService.findAll().subscribe(data=>{
-      //this.inscriptions = data;
       this.inscriptions = this.inscriptions.sort((a,b) => b.year - a.year);
       for(var inscription of data){
-        console.log(inscription.ue.durationExam);
-        console.log(Number(inscription.ue.durationExam)/60);
         inscription.ue.durationExam = Number(inscription.ue.durationExam)/60;
-        console.log(inscription.ue.durationExam);
         this.inscriptions.push(inscription);
       }
     });
