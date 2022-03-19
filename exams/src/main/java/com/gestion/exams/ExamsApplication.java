57237ba36a9f541785de59aeea1f8671d3bfa42c
Anthony <anthony_pro1998@outlook.fr>
2022-03-19 13:21:37 +0100

    remove useless import

--- exams/src/main/java/com/gestion/exams/ExamsApplication.java
+++ exams/src/main/java/com/gestion/exams/ExamsApplication.java
@@ -1,25 +1,14 @@
 package com.gestion.exams;
 
-import com.gestion.exams.entity.Authentification;
-import com.gestion.exams.entity.Role;
-import com.gestion.exams.entity.Student;
-import com.gestion.exams.repository.AuthentificationRepository;
-import com.gestion.exams.repository.RoleRepository;
-import com.gestion.exams.repository.StudentRepository;
-import com.gestion.exams.services.PopulateService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.gestion.exams.services.PopulateService;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = ExamsApplication.class)
