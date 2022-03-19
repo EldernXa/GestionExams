03dbc4d22a0058238730811ab93b5ff9e989e200
Anthony <anthony_pro1998@outlook.fr>
2022-03-19 13:23:39 +0100

    remove useless import

--- exams/src/main/java/com/gestion/exams/dto/mapper/ExamMapper.java
+++ exams/src/main/java/com/gestion/exams/dto/mapper/ExamMapper.java
@@ -1,29 +1,25 @@
 package com.gestion.exams.dto.mapper;
 
 import com.gestion.exams.dto.ExamDTO;
-import com.gestion.exams.dto.GradeDTO;
 import com.gestion.exams.entity.Exam;
-import com.gestion.exams.entity.Grade;
-import com.gestion.exams.repository.UERepository;
 import com.gestion.exams.services.ExamService;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class ExamMapper {
 
-    private static final ModelMapper modelMapper = new ModelMapper() ;
+	private static final ModelMapper modelMapper = new ModelMapper() ;
 
-    public static ExamDTO examToExamDTO(Exam exam) {
-        String mstNotRoomYet = "Pas de salle pour l'instant";
-        ExamDTO examDTO = modelMapper.map(exam, ExamDTO.class);
-        examDTO.setUe(exam.getUe().getName());
-        if(examDTO.getNameRoom() == null) {
-            examDTO.setNameRoom(mstNotRoomYet);
-        }
-        return examDTO;
-    }
+	public static ExamDTO examToExamDTO(Exam exam) {
+		String mstNotRoomYet = "Pas de salle pour l'instant";
+		ExamDTO examDTO = modelMapper.map(exam, ExamDTO.class);
+		examDTO.setUe(exam.getUe().getName());
+		if(examDTO.getNameRoom() == null) {
+			examDTO.setNameRoom(mstNotRoomYet);
+		}
+		return examDTO;
+	}
 
-    public Exam examDTOToExam(ExamDTO examDTO, ExamService examService) {
-        return examService.getExamById(examDTO.getIdExam()).get();
-    }
+	public Exam examDTOToExam(ExamDTO examDTO, ExamService examService) {
+		return examService.getExamById(examDTO.getIdExam()).get();
+	}
 }
