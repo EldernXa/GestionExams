6e82f89087214e37eac0a36158f8363a1b2680d4
thibaut <thi6aut@yahoo.fr>
2022-03-20 11:57:19 +0100

    suite test class created

--- exams/src/test/java/com/gestion/exams/ExamsApplicationTests.java
+++ exams/src/test/java/com/gestion/exams/ExamsApplicationTests.java
@@ -1,13 +1,22 @@
 package com.gestion.exams;
 
+import com.gestion.exams.controller.PeriodControllerTest;
+import com.gestion.exams.controller.UeControllerTest;
+import com.gestion.exams.services.GradeServiceTest;
+import com.gestion.exams.services.UeServiceTest;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+		GradeServiceTest.class,
+		UeServiceTest.class
+})
 
 @SpringBootTest
 class ExamsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
