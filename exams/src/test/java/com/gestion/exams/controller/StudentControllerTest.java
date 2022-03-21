52adeb18e43369cdc40b745df316e3c1bca3cd3f
bardaghjigeorges <georges.bardaghji@etu.univ-amu.fr>
2022-03-21 18:23:42 +0100

    Cleaning

--- exams/src/test/java/com/gestion/exams/controller/StudentControllerTest.java
+++ exams/src/test/java/com/gestion/exams/controller/StudentControllerTest.java
@@ -1,27 +1,17 @@
 package com.gestion.exams.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gestion.exams.entity.Authentification;
-import com.gestion.exams.repository.AuthentificationRepository;
-import com.gestion.exams.services.ExamService;
-import com.gestion.exams.services.StudentService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
