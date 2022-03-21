809afb7ef1e4fcc5074923261a3dada0d1c590cb
bardaghjigeorges <georges.bardaghji@etu.univ-amu.fr>
2022-03-21 18:04:29 +0100

    integration tests of StudentControllerTest

--- exams/src/test/java/com/gestion/exams/controller/StudentControllerTest.java
+++ exams/src/test/java/com/gestion/exams/controller/StudentControllerTest.java
@@ -1,8 +1,11 @@
 package com.gestion.exams.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gestion.exams.dto.ExamDTO;
+import com.gestion.exams.services.ExamService;
 import com.gestion.exams.services.StudentService;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +17,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.List;
 import java.util.Map;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,23 +34,36 @@ public class StudentControllerTest {
 
     @MockBean
     private StudentService studentService;
+    @Autowired
+    ExamService examService;
 
     String token;
 
     ObjectMapper mapper = new ObjectMapper();
+
     ModelMapper modelMapper = new ModelMapper();
+
+    MvcResult mvcResult;
 
     @Before
     public void authenticate() throws Exception {
-        MvcResult mvcResult =  mvc.perform(post("/login")
+        mvcResult =  mvc.perform(post("/login")
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .param("email", "school1@noteplus.fr")
-                        .param("password", "password2"))
-                .andExpect(status().isOk())
-                .andReturn();
+                        .param("email", "student1@noteplus.fr")
+                        .param("password", "password"))
+                        .andExpect(status().isOk())
+                        .andReturn();
         String json = mvcResult.getResponse().getContentAsString();
         Map<String, String> response = new ObjectMapper().readValue(json, Map.class);
         token = response.get("access_token");
         System.out.println(token);
     }
+
+    @Test
+    public void getNextPeriodOfExamTest() throws Exception {
+        mvc.perform(get("/exams").contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + token))
+                .andExpect(status().isOk());
+    }
+
 }
