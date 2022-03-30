1bad05ae0feaa890496e3ed4e821bddfe760a83a
thibaut <thi6aut@yahoo.fr>
2022-03-30 12:18:59 +0200

    updating populate and ue controller/service

--- exams/src/main/java/com/gestion/exams/services/PopulateService.java
+++ exams/src/main/java/com/gestion/exams/services/PopulateService.java
@@ -46,20 +46,36 @@ public class PopulateService{
 	@Autowired
 	private GradeRepository gradeRepository;
 
+	@Autowired
+	private UEService ueService;
+
+	@Autowired
+	private GradeService gradeService;
 
 
 	private SecureRandom random = new SecureRandom();
 
 	@PostConstruct
 	public void populate() {
+
 		populateRoom();
 		populateUE();
 		populateStudent();
 		populateAuthentification();
+		/*
 		populateInscription();
 		populatePeriod();
 		populateExam();
 		populateGrade();
+		 */
+		for(int year=2020; year<=2022; year++){
+			populateInscription(year);
+			populatePeriod(year);
+			populateExam(year,1);
+			populateGrade(year,1);
+			populateExam(year,2);
+			populateGrade(year,2);
+		}
 	}
 
 	private void populateRoom() {
@@ -87,9 +103,11 @@ public class PopulateService{
 	private void populateStudent() {
 		Faker faker = new Faker();
 		for(int i=0; i<10; i++) {
-			Student student = new Student(faker.name().firstName()+i, faker.name().lastName()+i, "student"+i+"@noteplus.fr");
+			Student student = new Student(faker.name().firstName(), faker.name().lastName(), "student"+i+"@noteplus.fr");
 			studentRepository.save(student);
 		}
+		Student student = new Student("Jean", "BADOGOLE", "student10@noteplus.fr");
+		studentRepository.save(student);
 	}
 
 
@@ -113,8 +131,6 @@ public class PopulateService{
 			authRepo.save(auth);
 		}
 
-		Authentification auth = new Authentification("test", passwordEncoder.encode("password3"), new ArrayList<>());
-		authRepo.save(auth);
 	}
 
 	private void populateInscription() {
@@ -144,6 +160,41 @@ public class PopulateService{
 			}
 		countUe++;
 		}
+
+	}
+
+	private void populateInscription(int year){
+		List<Student> students = studentRepository.findAll();
+
+		for(Student s : students) {
+			if(s.getLastName() != "BADOGOLE"){
+				List<UE> listUE = ueService.getSubscribeableInscriptionsOfStudent(s,year);
+				for(UE ue : listUE){
+					if(random.nextBoolean()){
+						Inscription inscription = new Inscription(s, year , ue);
+						inscriptionRepository.save(inscription);
+					}
+				}
+			}
+			else{
+				if(year == 2020){
+					Inscription inscription = new Inscription(s, year , ueService.getUeByName("Introduction à la programmation"));
+					inscriptionRepository.save(inscription);
+					inscription = new Inscription(s, year , ueService.getUeByName("Mathématique avancée"));
+					inscriptionRepository.save(inscription);
+				}
+				if(year == 2021){
+					Inscription inscription = new Inscription(s, year , ueService.getUeByName("Génie Logiciel"));
+					inscriptionRepository.save(inscription);
+					inscription = new Inscription(s, year , ueService.getUeByName("Mathématique avancée"));
+					inscriptionRepository.save(inscription);
+				}
+				if(year == 2022){
+					Inscription inscription = new Inscription(s, year , ueService.getUeByName("Optique"));
+					inscriptionRepository.save(inscription);
+				}
+			}
+		}
 	}
 
 	private void populatePeriod() {
@@ -164,6 +215,23 @@ public class PopulateService{
 		}
 	}
 
+	private void populatePeriod(int year) {
+			String strBeginDate1 = "01/01/"+year;
+			String strEndDate1 = "15/01/"+year;
+			String strBeginDate2 = "01/03/"+year;
+			String strEndDate2 = "15/03/"+year;
+			try {
+				Period period1 = new Period(new SimpleDateFormat("dd/MM/yyyy").parse(strBeginDate1),
+						new SimpleDateFormat("dd/MM/yyyy").parse(strEndDate1), "période session 1"+year);
+				periodRepository.save(period1);
+				Period period2 = new Period(new SimpleDateFormat("dd/MM/yyyy").parse(strBeginDate2),
+						new SimpleDateFormat("dd/MM/yyyy").parse(strEndDate2), "période session 2"+year);
+				periodRepository.save(period2);
+			}catch(Exception exception) {
+				LOGGER.log(Logger.Level.valueOf("context"),exception);
+			}
+	}
+
 	private void populateExam() {
 		List<Inscription> inscriptions = inscriptionRepository.findAll();
 		for(Inscription i : inscriptions) {
@@ -178,6 +246,22 @@ public class PopulateService{
 		}
 	}
 
+	private void populateExam(int year, int session) {
+		List<Inscription> inscriptions = inscriptionRepository.findAll();
+		for(Inscription i : inscriptions) {
+			if(i.getYear() == year) {
+				UE ue = i.getUe();
+				Period period = periodRepository.getPeriodByName("période session "+session+ " " + year).get(0);
+				List<Exam> exams = examRepository.searchExamsByUeAndYear(ue, year);
+				if (exams.isEmpty()) {
+					Exam exam = new Exam(null, null, 1, year, /*listRoom.get(0)*/null, period, ue);
+					examRepository.save(exam);
+				}
+			}
+		}
+	}
+
+
 	private void populateGrade() {
 
 		List<Inscription> inscriptions = inscriptionRepository.findAll();
@@ -187,6 +271,52 @@ public class PopulateService{
 				if(i.getUe().getName().equals(exam.getUe().getName()) && i.getYear() == exam.getYear()){
 						Grade grade = new Grade(i.getStudent(),exam, random.nextInt((20 ) + 1));
 						gradeRepository.save(grade);
+				}
+			}
+		}
+	}
+
+	private void populateGrade(int year, int session) {
+
+		List<Inscription> inscriptions = inscriptionRepository.findAll();
+
+		for(Exam exam : examRepository.findAll()) {
+			if(exam.getSession() == session && year != 2022)
+			for(Inscription i : inscriptions){
+				if(i.getStudent().getLastName() != "BADOGOLE"){
+					if(i.getUe().getName().equals(exam.getUe().getName()) && i.getYear() == exam.getYear() && i.getYear() == year){
+						if(session == 1 || (gradeService.getGradesMoreThan10ByStudentAndUE(i.getStudent().getIdStudent(),i.getUe().getName()).isEmpty() && session == 2)){
+							Grade grade = new Grade(i.getStudent(),exam, random.nextInt((20 ) + 1));
+							gradeRepository.save(grade);
+						}
+
+					}
+				}
+				else{
+					if(year == 2020 && i.getUe().getName().equals(exam.getUe().getName()) && i.getYear() == exam.getYear() && i.getYear() == year){
+						if(session == 1 && i.getUe().getName() == "Introduction à la programmation"){
+							Grade grade = new Grade(i.getStudent(),exam, 15);
+							gradeRepository.save(grade);
+						}
+						if(session == 1 && i.getUe().getName() == "Mathématique avancée"){
+							Grade grade = new Grade(i.getStudent(),exam, 5);
+							gradeRepository.save(grade);
+						}
+						if(session == 2 && i.getUe().getName() == "Mathématique avancée"){
+							Grade grade = new Grade(i.getStudent(),exam, 9);
+							gradeRepository.save(grade);
+						}
+					}
+					if(year == 2021 && i.getUe().getName().equals(exam.getUe().getName()) && i.getYear() == exam.getYear() && i.getYear() == year){
+						if(session == 1 && i.getUe().getName() == "Génie Logiciel"){
+							Grade grade = new Grade(i.getStudent(),exam, 18);
+							gradeRepository.save(grade);
+						}
+						if(session == 1 && i.getUe().getName() == "Mathématique avancée"){
+							Grade grade = new Grade(i.getStudent(),exam, 12);
+							gradeRepository.save(grade);
+						}
+					}
 				}
 			}
 		}
